c2f6e365b1f2d0a403caa66895d7112912bc6e6a
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-10 17:45:08 +0200

    3.0.0 The Menu Logo

--- bento/Sushisho/Sushisho.tsx
+++ bento/Sushisho/Sushisho.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Sushisho() {
   return (
     <motion.nav
-      className='drop-shadow-xl sticky max-w-6xl w-full bg-white-800 z-[1] filter-blur dark:bg-gray-800 top-2 md:top-4 px-4 py-2 rounded-md mx-auto flex justify-between items-center'
+      className='drop-shadow-xl max-w-6xl w-full bg-white-800 z-[1] filter-blur dark:bg-gray-800 top-2 md:top-4 px-4 py-2 rounded-md mx-auto flex justify-between items-center'
       animate={{ y: 25 }}
       transition={{ duration: 0.5 }}
     >
@@ -27,7 +27,13 @@ export default function Sushisho() {
       <div className='flex flex-row gap-1 text-tertiary'>
         <Link href='/'>
           <a className='hover:text-primary transition-all cursor-pointer'>
-            BCN HOSTESS
+            <div className='col-span-1 flex justify-center md:col-span-3 lg:col-span-1'>
+              <img
+                className='h-24'
+                src='https://raw.githubusercontent.com/miguelgargallo/SushiJS-Example-001/cf74497949cad84b0544e17b840cd5165c37545a/public/static/favicons/logo.svg'
+                alt='BCN Hostess'
+              />
+            </div>{' '}
           </a>
         </Link>
       </div>
--- next.config.js
+++ next.config.js
@@ -5,14 +5,4 @@ module.exports = {
   images: {
     domains: ['api.microlink.io'],
   },
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images : {
-    domains : ['media.geeksforgeeks.org']
-  }
-}
-  
-module.exports = nextConfig
 }
