bf7ddde3b95ec1b1b9860b78f81878d07711ccb2
miguelgargallo <mgargallo16@gmail.com>
2022-08-18 19:35:47 +0200

    1.8.0 Steve's NavBar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,22 @@ import Izakaya from 'bento/Izakaya'
 import ShashinArubamu from 'bento/ShashinArubamu'
 import Sushisho from 'bento/Sushisho'
 import Sushiten from 'bento/Sushiten'
+import Bottom from 'bento/Bottom'
+import Meta from 'bento/Meta'
+// import Appbar from 'bento/Appbar'
+// import Section from 'bento/Section'
+// import Page from 'bento/Page'
 
 export default function Home() {
   return (
     <>
+      <Meta />
       <Izakaya />
       <Sushisho />
       <Sushiten />
       <Bento />
       <ShashinArubamu />
+      <Bottom />
     </>
   )
 }
