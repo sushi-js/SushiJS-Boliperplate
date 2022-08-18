194bb772b49fcef75fe74e208975be319d1f7160
miguelgargallo <mgargallo16@gmail.com>
2022-08-18 21:25:48 +0200

    1.8.4.2 UI Improvements

--- bento/Bento/Bento.tsx
+++ bento/Bento/Bento.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import Logo from 'bento/Logo'
+
 export default function Bento() {
   return (
-    <div className='relative bg-white dark:bg-black pt-16 px-4 sm:px-6 lg:pt-24 lg:px-8'>
+    <div className='relative bg-white dark:bg-black pt-16 px-4 sm:px-6 lg:pt-12 lg:px-8'>
       <div className='bg-white dark:bg-black h-1/3 sm:h-2/3' />
       <div className='relative max-w-7xl mx-auto'>
         <div className='text-center'>
-          <h2 className='py-8 text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-white'>
+          <h2 className='py-0 text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-white'>
             Nuestros Clientes
           </h2>
           <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
