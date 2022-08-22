74e44812b0afb9457cdd95f6c8abb521de00bdaa
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 13:30:56 +0200

    1.0.52

--- src/pages/Ferias-Congresos.tsx
+++ src/pages/Ferias-Congresos.tsx
@@ -1,12 +1,13 @@
-import Bento from 'bento/05 WelcomeDescriptionText'
-import Izakaya from 'bento/11 ToggleDarkModeButton'
-import ShashinArubamu from 'bento/13 CardsPropsImproved'
-import Sushisho from 'bento/01 Menu'
-import Sushiten from 'bento/03 SecondaryMenu'
-import Bottom from 'bento/18 Bottom'
-import Meta from 'bento/19 Meta'
+import Bento from 'src/common/components/elements/02 Body/05 WelcomeDescriptionText'
+import Izakaya from 'src/common/components/elements/01 Head/11 ToggleDarkModeButton'
+import ShashinArubamu from 'src/common/components/elements/02 Body/13 CardsPropsImproved'
+import Sushisho from 'src/common/components/elements/01 Head/01 Menu'
+import Sushiten from 'src/common/components/elements/01 Head/03 SecondaryMenu'
+import Bottom from 'src/common/components/elements/02 Body/18 Bottom'
+import Meta from 'src/common/components/elements/00 Header/19 Meta'
+import Carrousel from 'src/common/components/elements/02 Body/22 Carrousel'
 // import Appbar from 'bento/Appbar'
-import Section from 'bento/20 Section'
+import Section from 'src/common/components/elements/02 Body/20 Section'
 // import Page from 'bento/Page'
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
       <Section />
       <Section />
       <Sushiten />
+      <Carrousel />
       <Bento />
       <ShashinArubamu />
       <Bottom />
