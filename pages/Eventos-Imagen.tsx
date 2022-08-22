57d78c7ca35c550b358c8e59a5723abde8adccda
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 11:48:29 +0200

    1.0.4

--- pages/Eventos-Imagen.tsx
+++ pages/Eventos-Imagen.tsx
@@ -1,12 +1,12 @@
-import Bento from 'bento/Bento'
-import Izakaya from 'bento/Izakaya'
+import Bento from 'bento/05 Bento'
+import Izakaya from 'bento/11 Izakaya'
 import ShashinArubamu from 'bento/13 ShashinArubamu'
-import Sushisho from 'bento/Sushisho'
-import Sushiten from 'bento/Sushiten'
+import Sushisho from 'bento/01 Sushisho'
+import Sushiten from 'bento/03 Sushiten'
 import Bottom from 'bento/18 Bottom'
 import Meta from 'bento/19 Meta'
 // import Appbar from 'bento/Appbar'
-// import Section from 'bento/Section'
+import Section from 'bento/20 Section'
 // import Page from 'bento/Page'
 
 export default function Home() {
@@ -15,6 +15,8 @@ export default function Home() {
       <Meta />
       <Izakaya />
       <Sushisho />
+      <Section />
+      <Section />
       <Sushiten />
       <Bento />
       <ShashinArubamu />
