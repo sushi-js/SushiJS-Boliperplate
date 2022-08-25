1ac4fc4cb136db4c6e865d2cc3beb9fe2a0883c1
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-25 10:44:27 +0200

    2.0.1 Tailwind Updated

--- src/uncommon/components/elementsbypage/Conocenos/02 Body/06 WelcomeDescriptionText/Bento.tsx
+++ src/uncommon/components/elementsbypage/Conocenos/02 Body/06 WelcomeDescriptionText/Bento.tsx
@@ -2,47 +2,21 @@ import React from 'react'
 
 export default function Bento() {
   return (
-    <div className='relative bg-white dark:bg-black pt-16 px-4 sm:px-6 lg:pt-12 lg:px-8'>
-      <div className='bg-white dark:bg-black h-1/3 sm:h-2/3' />
-      <div className='relative max-w-7xl mx-auto'>
-      <div className='py-8 text-center'>
-          <h1 className='py-0 text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-white'>
-          Maribel Risquez
-          </h1>
-          <h2 className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
-          1.988 así empezó          </h2>
-        
-          
-        </div>
-        <div className='py-8 text-center'>
-          <h2 className='py-0 text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-white'>
-            Conócenos
-          </h2>
-          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+    <div className="bg-white dark:bg-black">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-xl font-semibold text-black dark:text-white tracking-wide uppercase">1988</h2>
+          <p className="text-xl font-semibold text-black dark:text-white tracking-wide uppercase">
+            Maribel Risquez
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
           Barcelona Hostess fue fundada por Maribel Risquez en 1990, como una agencia especializada en azafatas y personal para todo tipo de eventos. Tras varios años de haber trabajado como azafata para importantes marcas, descubrió que también solventaba las necesidades de los mismos y fue así es como empezó a gestionar personal.
           </p>
-          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
-          La experiencia nos avala con más de 28 años de trayectoria, siendo una de las agencias más antiguas de España. Barcelona Hostess está formada por un equipo multidisciplinar, siendo actualmente una de las agencias líderes en servicios de imagen, protocolo y acciones de marketing.
-          </p>
-          
-        </div>
-        <div className='py-8 text-center'>
-          <h2 className='py-0 text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-white'>
-          ¿Que ofrecemos?
-          </h2>
-          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
-          Nuestra experiencia en el ámbito del trade marketing y eventos, nos permite ofrecer un servicio integral a empresas que requieran desde personal para eventos, congresos, ferias profesionales, promociones de producto, presentaciones, ruedas de prensa, actos deportivos, etc. hasta la organización detallada del mismo.
-Ayudamos y asesoramos a nuestros clientes a definir y solucionar sus retos necesarios para el éxito de sus marcas y sus negocios          </p>
-          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
-          Ofrecemos un proceso de selección acorde con las necesidades del cliente como:
-·Captación: web, anuncios, boca a boca.
-·Segmentación de perfiles
-·Formación: briefing, protocolos
-·Fidelización:Trato continuo y personalizado, incentivos por objetivos.
-Nuestro compromiso con cada cliente es total por lo cual ofrecemos atención 24Hs. y cobertura de cualquier imprevisto, garantizando así unos resultados óptimos.          </p>
-          
         </div>
       </div>
     </div>
   )
 }
+
+
+
